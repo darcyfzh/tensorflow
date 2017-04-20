4865192b920b445b079ed731de9cb1b5fa102a49
darcyfzh <1207217660@qq.com>
2017-04-20 23:43:35 +0800

    add 3 file

---
+++ mnist_part1/5_layers_sigmoid_learning_rate_decaying.py
@@ -0,0 +1,159 @@
+# encoding: UTF-8
+# Copyright 2016 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+tf.set_random_seed(0)
+import numpy as np
+import math
+import matplotlib.pyplot as plt 
+# neural network with 5 layers
+#
+# · · · · · · · · · ·          (input data, flattened pixels)       X [batch, 784]   # 784 = 28*28
+# \x/x\x/x\x/x\x/x\x/       -- fully connected layer (sigmoid)      W1 [784, 200]      B1[200]
+#  · · · · · · · · ·                                                Y1 [batch, 200]
+#   \x/x\x/x\x/x\x/         -- fully connected layer (sigmoid)      W2 [200, 100]      B2[100]
+#    · · · · · · ·                                                  Y2 [batch, 100]
+#     \x/x\x/x\x/           -- fully connected layer (sigmoid)      W3 [100, 60]       B3[60]
+#      · · · · ·                                                    Y3 [batch, 60]
+#       \x/x\x/             -- fully connected layer (sigmoid)      W4 [60, 30]        B4[30]
+#        · · ·                                                      Y4 [batch, 30]
+#         \x/               -- fully connected layer (softmax)      W5 [30, 10]        B5[10]
+#          ·                                                        Y5 [batch, 10]
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
+mnist = read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, 10])
+
+# five layers and their number of neurons (tha last layer has 10 softmax neurons)
+L = 200
+M = 100
+N = 60
+O = 30
+# Weights initialised with small random values between -0.2 and +0.2
+# When using RELUs, make sure biases are initialised with small *positive* values for example 0.1 = tf.ones([K])/10
+W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  # 784 = 28 * 28
+B1 = tf.Variable(tf.zeros([L]))
+W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
+B2 = tf.Variable(tf.zeros([M]))
+W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
+B3 = tf.Variable(tf.zeros([N]))
+W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(tf.zeros([O]))
+W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
+B5 = tf.Variable(tf.zeros([10]))
+
+# The model
+XX = tf.reshape(X, [-1, 784])
+Y1 = tf.nn.sigmoid(tf.matmul(XX, W1) + B1)
+Y2 = tf.nn.sigmoid(tf.matmul(Y1, W2) + B2)
+Y3 = tf.nn.sigmoid(tf.matmul(Y2, W3) + B3)
+Y4 = tf.nn.sigmoid(tf.matmul(Y3, W4) + B4)
+Ylogits = tf.matmul(Y4, W5) + B5
+Y = tf.nn.softmax(Ylogits)
+
+# cross-entropy loss function (= -sum(Y_i * log(Yi)) ), normalised for batches of 100  images
+# TensorFlow provides the softmax_cross_entropy_with_logits function to avoid numerical stability
+# problems with log(0) which is NaN
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
+cross_entropy = tf.reduce_mean(cross_entropy)
+#cross_entropy = -tf.reduce_sum(Y_*tf.log(Y))
+
+
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+
+
+# training step, learning rate
+lr = tf.placeholder(tf.float32)
+train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+#if use GradientDescentOptimizer,the accuracy will be very small about 0.11
+#train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy) 
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+correct_prediction = tf.equal(tf.argmax(Y,1), tf.argmax(Y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+acc_train = []
+acc_test = []
+cross_entropy_train = []
+cross_entropy_test = []
+def trainingAndTesting(max_learning_rate,min_learning_rate,epochs,decay_speed):
+    # max_learning_rate = 0.003
+    # min_learning_rate = 0.0001
+    # decay_speed = 2000.0
+    #learning rate decaying in the process
+    for i in range(epochs):
+      batch_xs, batch_ys = mnist.train.next_batch(100)
+      acc_train.append(sess.run(accuracy, feed_dict={X: batch_xs, Y_: batch_ys}))
+      cross_entropy_train.append(sess.run(cross_entropy, feed_dict={X: batch_xs, Y_: batch_ys}))
+      learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-i/decay_speed)
+      sess.run(train_step, feed_dict={X: batch_xs, Y_: batch_ys, lr: learning_rate})
+      acc_test.append(sess.run(accuracy, feed_dict={X: mnist.test.images, Y_: mnist.test.labels}))
+      cross_entropy_test.append(sess.run(cross_entropy, feed_dict={X: mnist.test.images, Y_: mnist.test.labels}))
+    axis_X = range(epochs)
+    fig1 = plt.subplot(221)
+    fig2 = plt.subplot(222)
+    fig3 = plt.subplot(223)
+    fig4 = plt.subplot(224)
+    plt.plot(axis_X, acc_train)
+    plt.title('train_accuracy')
+    plt.sca(fig1)
+
+    plt.plot(axis_X, acc_test)
+    plt.title('test_accuracy')
+    plt.sca(fig2)
+
+    plt.plot(axis_X, cross_entropy_train)
+    plt.title('cross_entropy_train')
+    plt.sca(fig3)
+
+    plt.plot(axis_X, cross_entropy_test)
+    plt.title('cross_entropy_test')
+    plt.sca(fig4)
+    plt.show()
+
+trainingAndTesting(0.003,0.0001,100,2000)
+#tf.argmax(y,1)返回的是模型对于任一输入x预测到的标签值，而 tf.argmax(y_,1) 代表正确的标签，
+#用 tf.equal 来检测我们的预测是否真实标签匹配(索引位置一样表示匹配)。
+
+
+#为了确定正确预测项的比例，我们可以把布尔值转换成浮点数，然后取平均值。
+#例如，[True, False, True, True] 会变成 [1,0,1,1]
+
+# You can call this function in a loop to train the model, 100 images at a time
+# You can call this function in a loop to train the model, 100 images at a time
+# (In all runs, if sigmoids are used, all biases are initialised at 0, if RELUs are used,
+# all biases are initialised at 0.1 apart from the last one which is initialised at 0.)
+
+## learning rate = 0.003, 10K iterations
+# final test accuracy = 0.9788 (sigmoid - slow start, training cross-entropy not stabilised in the end)
+# final test accuracy = 0.9825 (relu - above 0.97 in the first 1500 iterations but noisy curves)
+
+## now with learning rate = 0.0001, 10K iterations
+# final test accuracy = 0.9722 (relu - slow but smooth curve, would have gone higher in 20K iterations)
+
+## decaying learning rate from 0.003 to 0.0001 decay_speed 2000, 10K iterations
+# final test accuracy = 0.9746 (sigmoid - training cross-entropy not stabilised)
+# final test accuracy = 0.9824 (relu - training set fully learned, test accuracy stable)
